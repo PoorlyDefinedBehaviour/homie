db83cbd092bc1f7a7f74d1fd3d710b3693cfbfad
Bruno <brunotj2015@hotmail.com>
2019-07-01 01:30:24 -0300

    its now possible to add a command

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,24 +1,31 @@
 import * as Discord from "discord.js";
 
+import { Lexer } from "../Lexer/Index";
+
 import {
   is_prefix_valid,
-  is_command_name_valid
+  is_command_name_valid,
+  is_action_valid,
+  command_exists
 } from "./validators/Validators";
 
-import { InvalidPrefix, InvalidCommandName } from "./errors/Errors";
-
-interface Command {
-  name: string;
-  action: any;
-}
+import {
+  InvalidPrefix,
+  InvalidCommand,
+  InvalidAction,
+  CommandExists
+} from "./errors/Errors";
 
 class Client {
   private _instance: Discord.Client;
-  private _prefix: string = "";
+  private _prefix: string = "!";
+  private _commands: Map<string, any> = new Map<string, any>();
 
   constructor(private readonly bot_token: string) {
     this._instance = new Discord.Client();
     this._instance.login(this.bot_token);
+
+    this._instance.on("message", this.handle_messages);
   }
 
   public set prefix(prefix: string) {
@@ -26,8 +33,19 @@ class Client {
     this._prefix = prefix;
   }
 
-  public command(command_name: string, action: any): void {
-    if (!is_command_name_valid(command_name)) InvalidCommandName.throw();
+  public command(command: string, action: any): void {
+    if (command_exists(this._commands, command)) CommandExists.throw();
+    if (!is_command_name_valid(command)) InvalidCommand.throw();
+    if (!is_action_valid(action)) InvalidAction.throw();
+
+    this._commands.set(command, action);
+    this.add_listener(command);
+  }
+
+  private add_listener(command: string): void {}
+
+  private handle_messages(message: Discord.Message) {
+    const tokens: Array<string> = Lexer.tokenize(message);
   }
 }
 
