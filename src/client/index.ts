d99e8ed224b0942c1692b2c0103a280a4db122a2
Bruno <brunotj2015@hotmail.com>
2019-06-30 23:57:57 -0300

    creates prefix setter and prefix validation

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,18 +1,24 @@
 import * as Discord from "discord.js";
 
+import { InvalidPrefixError } from "./errors/Errors";
+
 class Client {
   private _instance: Discord.Client;
+  private _prefix: string = "";
 
   constructor(private readonly bot_token: string) {
     this._instance = new Discord.Client();
     this._instance.login(this.bot_token);
-    console.log(this._instance);
   }
 
-  public async connect(): Promise<Boolean> {
-    console.log("connecting...", this.bot_token);
+  public set prefix(prefix: string) {
+    if (!this.is_prefix_valid(prefix)) InvalidPrefixError.throw();
+    this._prefix = prefix;
+  }
 
-    return true;
+  private is_prefix_valid(prefix: string): boolean {
+    const trimmed_prefix: string = prefix.replace(new RegExp(" ", "g"), "");
+    return trimmed_prefix.length > 0 && trimmed_prefix.length < 4;
   }
 }
 
