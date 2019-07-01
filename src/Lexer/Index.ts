50789d060d45cf22de6d8adac3b3834baa3a8e0e
Bruno <brunotj2015@hotmail.com>
2019-07-01 01:57:03 -0300

    tokenize returns an object instead of an array

--- src/Lexer/Index.ts
+++ src/Lexer/Index.ts
@@ -1,55 +1,29 @@
-interface Token {
-  key: string;
-  value: string;
-}
-
 export class Lexer {
-  static tokenize(message: any): Array<string> {
-    let tokens: Array<Token> = [];
+  static tokenize(message: any): any {
+    let Token: any = {};
 
-    console.log(message);
+    Token["ChannelId"] = message.channel.id;
+    Token["ChannelType"] = message.channel.type;
+    Token["ChannelName"] = message.channel.name;
+    Token["ChannelParentId"] = message.channel.parentID;
+    Token["ChannelLastMessageId"] = message.channel.lastMessageID;
 
-    tokens.push({ key: "ChannelId", value: message.channel.id });
-    tokens.push({ key: "ChannelType", value: message.channel.type });
-    tokens.push({ key: "ChannelName", value: message.channel.name });
-    tokens.push({ key: "ChannelParentId", value: message.channel.parentID });
-    tokens.push({
-      key: "ChannelLastMessageId",
-      value: message.channel.lastMessageID
-    });
+    Token["ServerId"] = message.channel.guild.id;
+    Token["ServerName"] = message.channel.guild.name;
+    Token["ServerRegion"] = message.channel.guild.region;
+    Token["ServerJoinedTimestamp"] = message.channel.guild.joinedTimestamp;
 
-    tokens.push({ key: "ServerId", value: message.channel.guild.id });
-    tokens.push({ key: "ServerName", value: message.channel.guild.name });
-    tokens.push({ key: "ServerRegion", value: message.channel.guild.region });
-    tokens.push({
-      key: "ServerJoinedTimestamp",
-      value: message.channel.guild.joinedTimestamp
-    });
-    tokens.push({ key: "ServerOwnerId", value: message.channel.guild.ownerID });
+    Token["ServerOwnerId"] = message.channel.guild.ownerID;
 
-    tokens.push({ key: "MessageId", value: message.id });
-    tokens.push({ key: "MessageType", value: message.type });
-    tokens.push({ key: "MessageContent", value: message.content });
-    tokens.push({ key: "MessageAuthorId", value: message.author.id });
-    tokens.push({
-      key: "MessageAuthorUsername",
-      value: message.author.username
-    });
-    tokens.push({
-      key: "MessageAuthorDiscriminator",
-      value: message.author.discriminator
-    });
-    tokens.push({
-      key: "MessageAuthorLastMessageId",
-      value: message.author.lastMessageID
-    });
-    tokens.push({
-      key: "MessageAuthorLastMessageId",
-      value: message.author.lastMessageID
-    });
+    Token["MessageId"] = message.id;
+    Token["MessageType"] = message.type;
+    Token["MessageContent"] = message.content.split(" ");
+    Token["MessageAuthorId"] = message.author.id;
+    Token["MessageAuthorUsername"] = message.author.username;
+    Token["MessageAuthorDiscriminator"] = message.author.discriminator;
+    Token["MessageAuthorLastMessageId"] = message.author.lastMessageID;
+    Token["MessageAuthorLastMessageId"] = message.author.lastMessageID;
 
-    console.log(tokens);
-
-    return [];
+    return Token;
   }
 }
