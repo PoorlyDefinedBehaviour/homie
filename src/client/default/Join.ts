28426792ea20638a834ef943e56d49ca92558dae
Bruno <brunotj2015@hotmail.com>
2019-07-02 23:32:46 -0300

    bot only joins if not in the channel already

--- src/client/default/Join.ts
+++ src/client/default/Join.ts
@@ -1,5 +1,13 @@
 import { Message } from "discord.js";
+import { Optional } from "../interfaces";
 
 export default async (client: any, message: Message): Promise<void> => {
-  client.connection = await message.member.voiceChannel.join();
+  const user_channel_id: number = (message.member
+    .voiceChannelID as unknown) as number;
+
+  const { value: bot_channel_id }: Optional<number, null> = client.channel_id;
+
+  if (user_channel_id !== bot_channel_id)
+    client.connection = await message.member.voiceChannel.join();
+  else message.reply("I'm already in your channel");
 };
