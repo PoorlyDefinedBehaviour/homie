5f334835db0665d3ae752c20d1685ead944c8e85
Bruno <brunotj2015@hotmail.com>
2019-07-03 02:07:17 -0300

    adds stop command

--- src/client/default/Stop.ts
+++ src/client/default/Stop.ts
@@ -1,3 +1,6 @@
 import { Message } from "discord.js";
 
-export default (client: any, _: Message): void => {};
+export default (client: any, _: Message): void => {
+  client.reset_queue();
+  client.end_dispatcher();
+};
