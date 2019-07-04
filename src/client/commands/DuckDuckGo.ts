b81169e3db2690e308da6a7f0ba90759e3f42e7d
Bruno <brunotj2015@hotmail.com>
2019-07-04 17:53:04 -0300

    adds !duckduckgo command

---
+++ src/client/commands/DuckDuckGo.ts
@@ -0,0 +1,45 @@
+import request from "request-promise";
+import { Message } from "discord.js";
+import { Optional } from "../types";
+import { get_args } from "../utils";
+
+export default async (client: any, message: Message): Promise<void> => {
+  const search_terms: Optional<Array<string>, null> = get_args(message);
+
+  if (!search_terms) {
+    message.reply("You need to provide something for me to search for.");
+    return;
+  }
+
+  const response: any = await request({
+    method: "GET",
+    url: "http://api.duckduckgo.com/",
+    json: true,
+    qs: {
+      q: `${search_terms.join(" ")}`,
+      format: "json",
+      pretty: "1",
+      no_redirect: "1",
+      skip_disambig: "0",
+      t: "Homie"
+    }
+  }).catch(error => {
+    console.error(error);
+    message.reply(
+      `Something went wrong trying to search for \`${search_terms.join(" ")}\`.`
+    );
+  });
+
+  if (!response)
+    message.reply(
+      `Nothing was found when searching for \`${search_terms.join(" ")}\``
+    );
+
+  console.log(response);
+
+  if (response.Results.length > 0) {
+    message.channel.send(response.Results[0].FirstURL);
+  } else {
+    message.channel.send(response.RelatedTopics[0].FirstURL);
+  }
+};
