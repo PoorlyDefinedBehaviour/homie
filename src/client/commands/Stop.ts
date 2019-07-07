15093239d1bea1351d13b4591060b5a1f363da34
Bruno <brunotj2015@hotmail.com>
2019-07-06 21:51:10 -0300

    adds null checks for !stop command

--- src/client/commands/Stop.ts
+++ src/client/commands/Stop.ts
@@ -2,6 +2,8 @@ import { Message } from "discord.js";
 
 export default (client: any, message: Message): void => {
   client.reset_queue();
-  client.dispatcher.end();
+  if (client.dispatcher) {
+    client.dispatcher.end();
+  }
   message.reply("Cleaned playlist");
 };
