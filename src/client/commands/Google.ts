0e9e07309e34461d5fb164dac94750c995418e20
Bruno <brunotj2015@hotmail.com>
2019-07-06 21:51:53 -0300

    !google sends the link as answer instead of an embed

--- src/client/commands/Google.ts
+++ src/client/commands/Google.ts
@@ -1,9 +1,9 @@
 import request from "request-promise";
-import { Message, RichEmbed } from "discord.js";
+import { Message } from "discord.js";
 import { Optional } from "../types";
 import { get_args } from "../utils";
 
-export default async (client: any, message: Message): Promise<void> => {
+export default async (_: any, message: Message): Promise<void> => {
   const search_terms: Optional<Array<string>, null> = get_args(message);
 
   if (!search_terms) {
@@ -35,15 +35,11 @@ export default async (client: any, message: Message): Promise<void> => {
 
   const [result]: any = response.items;
 
-  const embed = new RichEmbed()
-    .setColor("#0066CC")
-    .setURL(result.link)
-    .setTitle(result.title)
-    .setDescription(result.snippet)
-    .setFooter(result.link, result.link);
-
-  if (result.pagemap && result.pagemap.cse_thumbnail)
-    embed.setThumbnail(result.pagemap.cse_thumbnail[0].src);
-
-  message.channel.send({ embed });
+  if (result) {
+    message.reply(result.link);
+  } else {
+    message.reply(
+      `Something went wrong trying to search for \`${search_terms.join(" ")}\``
+    );
+  }
 };
